63641f74006329f060f75904dbc462cfaf30df06
Kathryn Kananen <Kekananen@gmail.com>
2023-11-27 21:50:24 -0500

    Update microbeannotator_commands.sh

--- microbeannotator_commands.sh
+++ microbeannotator_commands.sh
@@ -1,1 +1,4 @@
 microbeannotator -i genomes/Vibrio_cholerae_RFB16.faa -o microbeannotator/Vibrio_cholerae_RFB16/ -m blast -d /fs/project/bradley.720/db/microbeannotator/ -t 40
+microbeannotator -i genomes/Bradyrhizobium_manausense_BR3351.faa -o microbeannotator/Bradyrhizobium_manausense_BR3351/ -m blast -d /fs/project/bradley.720/db/microbeannotator/ -t 40
+microbeannotator -i genomes/Robinsoniella_peoriensis_B23985.faa -o microbeannotator/Robinsoniella_peoriensis_B23985/ -m blast -d /fs/project/bradley.720/db/microbeannotator/ -t 40
+microbeannotator -i genomes/Lactiplantibacillus_plantarum_SRCM100442.faa -o microbeannotator/Lactiplantibacillus_plantarum_SRCM100442/ -m blast -d /fs/project/bradley.720/db/microbeannotator/ -t 40
