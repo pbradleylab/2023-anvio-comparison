45eccb71aa2659186ee071b59632683509cf5356
Iva Veseli <iva.veseli@gmail.com>
2024-11-01 14:44:50 +0100

    make sure this script manages the per-genome operon table

--- workflow/scripts/find_operon_examples.sh
+++ workflow/scripts/find_operon_examples.sh
@@ -13,12 +13,13 @@ if [ -z "$1" ]; then
   exit 1
 fi
 FUNCTIONS_OUTPUT_DIR=$1
+OPERONS_IN_GENOMES="${FUNCTIONS_OUTPUT_DIR}/operons_in_genomes.txt"
 
 TOOL_DIR_1="anvio"
 TOOL_DIR_2="kofamscan"
 OUTPUT_TABLE="${FUNCTIONS_OUTPUT_DIR}/${TOOL_DIR_1}-vs-${TOOL_DIR_2}-operon-examples.txt"
 
-rm -f $OUTPUT_TABLE
+echo -e "genome\ttool\tmodules_in_operon" > $OPERONS_IN_GENOMES # this output will be appended to by `find_and_compare_operons_in_modules.py``
 
 for dir in ${FUNCTIONS_OUTPUT_DIR}/${TOOL_DIR_1}/metabolism/default/*/; do \
     dir=${dir%*/}      # remove the trailing "/"
@@ -36,4 +37,4 @@ for dir in ${FUNCTIONS_OUTPUT_DIR}/${TOOL_DIR_1}/metabolism/default/*/; do \
     fi
 done
 
-echo "Operon examples are printed to $OUTPUT_TABLE"
+echo "Modules with operon-like structure in each genome are described in $OPERONS_IN_GENOMES"
